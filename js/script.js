02390ef15223615d708d0792a479c1ade5ef1ff6
J. Doig <33632968+eldoigo@users.noreply.github.com>
2018-04-08 02:58:22 +0100

    Version1
    js and flexbox

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+
+$(document).ready(function () {
+   'use strict';
+	$('header').fadeIn(2000).removeClass('hidden');
+	$('.box1').fadeIn(2000).removeClass('hidden');
+	$('.box2').fadeIn(3000).removeClass('hidden');
+	$('.box3').fadeIn(4000).removeClass('hidden');
+});
