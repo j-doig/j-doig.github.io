562b7ae849641f7d2277aa080000474fe96e245d
J. Doig <33632968+eldoigo@users.noreply.github.com>
2018-04-09 23:24:50 +0100

    ver.10
    l

--- js/script.js
+++ js/script.js
@@ -7,4 +7,18 @@ $(document).ready(function () {
 	$('.box1').fadeIn(2000).removeClass('hidden');
 	$('.box2').fadeIn(3000).removeClass('hidden');
 	$('.box3').fadeIn(4000).removeClass('hidden');
+	$('.breaker1 p').fadeIn(4000).removeClass('hidden');
+});
+
+$(document).ready(function() {   
+    'use strict';
+    $(window).scroll( function(){   
+        $('.hideme').each( function(){ 
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();     
+            if( bottom_of_window > bottom_of_object ){      
+                $(this).animate({'opacity':'1'},1000);               
+            }
+        }); 
+    }); 
 });
